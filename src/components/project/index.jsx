21cff1fffd4dfff0518b65628acc3386eb59eedf
Damien Decelle <80416124+damiendecelle@users.noreply.github.com>
2023-07-25 14:35:24 +0200

    Update index.jsx

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -135,7 +135,7 @@ const Project = ({ repo, loading, github, googleAnalytics }) => {
                       skeleton({ width: 'w-40', height: 'h-8' })
                     ) : (
                       <span className="text-base-content opacity-70">
-                        Mes projets GitHub
+                        Mes Projets GitHub
                       </span>
                     )}
                   </h5>
